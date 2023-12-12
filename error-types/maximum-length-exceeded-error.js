39480cf0320f2aa50fbb2e8c0b58d7d3562d3a17
JBasias <jbasias@gmail.com>
2023-12-12 18:20:59 -0500

    a much needed commit

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,5 +1,31 @@
 const ValidationError = require('./validation-error');
 
+class MaximumLengthExceededError extends ValidationError{
+  constructor(val = null , ...params) {
+      // Pass remaining arguments to parent constructor
+      super(...params);
+
+
+      this.name = "MaximumLengthExceededError";
+
+      // Maintains proper stack trace for where error was thrown (available on V8)
+      if (Error.captureStackTrace) {
+          Error.captureStackTrace(this, MaximumLengthExceededError  );
+      }
+
+      // The name property should match the class's name
+      //this.name = 'MissingRequiredFieldError';
+
+      // Custom debugging information
+      //this.fieldName = fieldName;
+
+      if(val === null) this.message =  "Maximum length exceeded";
+      else this.message = "Maximum length exceeded by " + val;
+      //this.message = message || "Invalid input";
+      //this.date = new Date();
+  }
+}
+
 // Your code here
 
 /****************************************************************************/
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,5 +1,30 @@
 // Your code here
 
+class ValidationError extends Error {
+  constructor(message = null , ...params) {
+      // Pass remaining arguments to parent constructor
+      super(...params);
+
+
+      this.name = "ValidationError";
+
+      // Maintains proper stack trace for where error was thrown (available on V8)
+      if (Error.captureStackTrace) {
+          Error.captureStackTrace(this, ValidationError);
+      }
+
+      // The name property should match the class's name
+      //this.name = 'MissingRequiredFieldError';
+
+      // Custom debugging information
+      //this.fieldName = fieldName;
+
+      this.message = message !== null ? message : "Invalid input";
+      //this.message = message || "Invalid input";
+      //this.date = new Date();
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
